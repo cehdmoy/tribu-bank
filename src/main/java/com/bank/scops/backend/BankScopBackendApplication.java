f8feb35c2268d932cd5f5fa1d084066b50111333
Roberto Moya Suárez <roberto.moya.suarez@gmail.com>
2019-02-18 10:39:08 -0300

    Change en lombok version to make it compile by terminal

--- src/main/java/com/bank/scops/backend/BankScopBackendApplication.java
+++ src/main/java/com/bank/scops/backend/BankScopBackendApplication.java
@@ -1,10 +1,11 @@
 package com.bank.scops.backend;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class BankScopBackendApplication {
 
     public static void main(String[] args) {
