46dbbe2eb8c838bc3363f8d58752e319ba2ecd2f
Roberto Moya Suárez <roberto.moya.suarez@gmail.com>
2019-03-08 15:32:09 -0300

    Remove non needed spring context for unit test

--- src/test/java/com/bank/scops/backend/mapper/AmountFormatterTest.java
+++ src/test/java/com/bank/scops/backend/mapper/AmountFormatterTest.java
@@ -1,19 +1,16 @@
 package com.bank.scops.backend.mapper;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.mockito.InjectMocks;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = AmountFormatter.class)
 public class AmountFormatterTest {
-    @Autowired
-    private AmountFormatter amountFormatter;
+
+    @InjectMocks
+    private AmountFormatter amountFormatter = new AmountFormatter();
 
     @Test
     public void isClassImpThere() {
